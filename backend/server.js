70ffb86a54f82aef252cb87e0d002005ddf48e51
JakubWesoly <jakub.wesoly@outlook.com>
2023-01-10 22:53:34 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use('/api/search', searchRoutes);
 
 // const pathname = path.dirname(new URL(import.meta.url).pathname);
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 );
